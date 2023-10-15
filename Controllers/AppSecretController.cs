12d890629460587eb96b628f113f9fc22004a2ac
jayant <jayanttripathy1985@gmail.com>
2023-10-15 19:15:42 +0530

    Add project files.

---
+++ Controllers/AppSecretController.cs
@@ -0,0 +1,24 @@
+﻿using AppSecrets.API.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AppSecrets.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AppSecretController : ControllerBase
+    {
+        private readonly AppConfiguration _config;
+
+        public AppSecretController(AppConfiguration config)
+        {
+            this._config = config;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(this._config);
+        }
+    }
+}
---
+++ Models/AppConfiguration.cs
@@ -0,0 +1,13 @@
+﻿namespace AppSecrets.API.Models
+{
+    public class AppConfiguration
+    {
+        public string ApiKey { get; set; }
+        public ApiHost ApiHost { get; set; }
+    }
+    public class ApiHost
+    {
+        public string Url { get; set; }
+        public string ApiKey { get; set; }
+    }
+}
